1b7a5baf5a7a5333bded9d8e831fbd233118083a
neonll <neon.bmstu@gmail.com>
2020-06-07 04:37:10 +0300

    dnf: add GetCoulombData.php

---
+++ app/Jobs/GetCoulombData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class GetCoulombData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $url = "http://" . env('COULOMB_IP') . "/data.jsn";
+        $json = substr(str_replace("\r\n", '', file_get_contents($url)), 3);
+        $data = json_decode($json, true);
+    }
+}
